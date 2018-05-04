1679d98d218ab56732d8809eefbe9efeeb7c71d6
grantyang <grantyang1@gmail.com>
2018-05-04 15:18:59 -0700

    testing chatterbox

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,5 +1,7 @@
 // YOUR CODE HERE:
 // Server: http://parse.sfm6.hackreactor.com/
+// escape notes: &, <, >, ", ', `, , !, @, $, %, (, ), =, +, {, }, [, and ] 
+// http://parse.sfm6.hackreactor.com/chatterbox/classes/messages
 //
 // $.ajax({
 //   // This is the url you should use to communicate with the parse API server.
@@ -15,3 +17,15 @@
 //     console.error('chatterbox: Failed to send message', data);
 //   }
 // });
+
+$.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+  type: 'GET',
+  contentType: 'application/json',
+  success: console.log(data),
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: GET request failed', data);
+  }
+});
